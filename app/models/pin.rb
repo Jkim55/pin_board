05e081264d31158341b4109a514705408d6a0ffb
Ji Kim <jkim55@gmail.com>
2016-11-30 01:35:45 -0700

    styled all forms

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ApplicationRecord
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { large: "750x750>" }
+  has_attached_file :image, styles: { large: "750x750>", thumb: "100x100>"  }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
